35eb708d91db5837ec2950e8443eb4e0816cc520
Olivier Van hamme <nicefiction@gmail.com>
2021-04-06 09:51:34 +0200

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Olivier Van hamme on 06/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Olivier Van hamme on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
