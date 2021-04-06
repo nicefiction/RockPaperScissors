4e45e715e85c270eb8c2fdd37ccf3f51818efe47
Olivier Van hamme <nicefiction@gmail.com>
2021-04-06 16:35:18 +0200

    Completes the Rock Paper Scissors app , and Day 25 .

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -1,12 +1,6 @@
 // MARK: ContentView.swift
 
-
-
- // ////////////////
-//  MARK: LIBRARIES
-
 import SwiftUI
-
 
 
 
@@ -15,13 +9,118 @@ import SwiftUI
 
 struct ContentView: View {
     
+     // ////////////////////////
+    //  MARK: PROPERTY WRAPPERS
+    
+    @State private var isShowingGameRoundOutcomeAlert: Bool = false
+    @State private var rockPaperScissors: [String] = [ "⛰" , "📄" , "✂️" ]
+    @State private var playerASelectionIndex: Int = Int.random(in : 0...2)
+    @State private var playerBSelectionIndex: Int = 0
+    @State private var gameScore: Int = 0
+    @State private var gameRound: Int = 1
+ 
+    
+    
      // //////////////////////////
     //  MARK: COMPUTED PROPERTIES
     
+    var roundScore: Int {
+        
+        switch (rockPaperScissors[playerASelectionIndex] ,
+                rockPaperScissors[playerBSelectionIndex]) {
+        
+        case ("⛰" , "📄") : return 1
+        case ("⛰" , "✂️") : return -1
+        case ("📄" , "✂️") : return 1
+        case ("📄" , "⛰") : return -1
+        case ("✂️" , "⛰") : return 1
+        case ("✂️" , "📄") : return -1
+        default            : return 0
+        }
+    }
+    
+    
+    var roundFeedback: String {
+        
+        switch roundScore {
+        
+        case 1  : return "You win ."
+        case 0  : return "It is a Draw ."
+        default : return "You lose ."
+        }
+    }
+    
+    
     var body: some View {
         
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing : 15) {
+            // Text("Player A has chosen \(rockPaperScissors[playerASelectionIndex]) .")
+            Text("Player A has chosen . Your turn :")
+            Spacer()
+            ForEach(0..<rockPaperScissors.count) { (index: Int) in
+                
+                Button(action : {
+                    tapButtonWith(index : index)
+                } , label : {
+                    ZStack {
+                        Circle()
+                            .foregroundColor(Color.orange)
+                        Text(rockPaperScissors[index])
+                            .font(.system(size: 80.0))
+                    }
+                })
+                .alert(isPresented : $isShowingGameRoundOutcomeAlert) {
+                    Alert(title : Text(roundFeedback) ,
+                          message : Text("Your new game score : \(gameScore)") ,
+                          dismissButton : .default(Text("Continue")) {
+                            
+                            startNewRound()
+                          })
+                }
+            }
+           
+            VStack(spacing : 12) {
+                Text("Your score : \(gameScore)")
+                Text("Round \(gameRound) of 10")
+            }
+            .font(.subheadline)
+        }
+    }
+    
+    
+    
+     // /////////////////////
+    //  MARK: HELPER METHODS
+    
+    func startNewRound() {
+        
+        gameRound < 10 ? gameRound += 1 : startNewGame()
+        playerASelectionIndex = Int.random(in: 0...2)
+    }
+    
+    
+    func startNewGame() {
+        
+        gameRound = 0
+        gameScore = 0
+        playerASelectionIndex = Int.random(in: 0...2)
+    }
+    
+    
+    func updateGameScore(with score: Int) {
+        
+        gameScore += score
+    }
+    
+    
+    func tapButtonWith(index: Int) {
+        
+        print("\(rockPaperScissors[index]) has been tapped .")
+        
+        playerBSelectionIndex = index
+        updateGameScore(with : roundScore)
+        
+        isShowingGameRoundOutcomeAlert.toggle()
     }
 }
 
